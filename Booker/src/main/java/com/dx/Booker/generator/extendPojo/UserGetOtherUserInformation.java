3e2a063be922179f97207bb20fa6f14153800e31
产品研发中心-项目组-钟湘 <zhong.xiang@cpt.com>
2018-09-03 15:53:04 +0800

    zx

---
+++ Booker/src/main/java/com/dx/Booker/generator/extendPojo/UserGetOtherUserInformation.java
@@ -0,0 +1,10 @@
+package com.dx.Booker.generator.extendPojo;
+
+/**
+ * @program: Booker
+ * @description: ${description}
+ * @author: zhong.xiang
+ * @create: 2018-09-03 13:31
+ **/
+public class UserGetOtherUserInformation {
+}
